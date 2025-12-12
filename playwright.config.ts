c8e01152b54be958c07e8afca20e3ce456da078e
Varalakshmidvl <varalakshmi.doddi@gmail.com>
2025-12-12 22:29:42 +0530

    New Merge code

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,62 +1,51 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
- // retries: process.env.CI ? 2 : 0,
+  //retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  //reporter:[ ['html',{outputFile:'test-results.html'}],["allure-playwright"]],
-   reporter: [
+
+  reporter: [
     ['html'],
     ['list'],
     ['allure-playwright'],
     ['playwright-html-reporter', { 
       testFolder: 'tests',
       title: 'OPEN CART HTML Report',
-      project: 'NALPOMNOV25',
+      project: 'Open Cart',
       release: '9.87.6',
-      testEnvironment: 'QA',
+      testEnvironment: 'PROD',
       embedAssets: true,
       embedAttachments: true,
       outputFolder: 'playwright-html-report',
       minifyAssets: true,
       startServer: false,
     }]
-
   ],
-
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  
   use: {
     
     trace: 'on-first-retry',
     headless: true,
-    //headless: !!process.env.CI,  // false locally, true in CI
-    screenshot:'on-first-failure',  // only-on-failure',
-    video :'on',
-    baseURL:'https://naveenautomationlabs.com/opencart/index.php',
-             
+    screenshot: 'on',
+    video: 'on',
+    baseURL: 'https://naveenautomationlabs.com/opencart/index.php',
+    
   },
-  
-   metadata:{
-    appUsername:'varalakshmi.d@gmail.com',
-    appPassword:'Hydchn66@@'
-   },
+
+  metadata: {
+    appUsername: 'pwtest@nal.com',
+    appPassword: 'test123'
+  },
 
   /* Configure projects for major browsers */
-//   projects: [
-//     {
-//       name: 'chromium',
-//       use: { ...devices['Desktop Chrome'] },
-//     },
-// /* 
-   projects: [
+  projects: [
   {
     name: 'Google Chrome',
     use: {
@@ -68,55 +57,55 @@ export default defineConfig({
       }
     }
   },
-/*
-  {
-    name: 'Microsoft Edge',
-    use: {
-      channel: 'msedge',
-      viewport: null,
-      launchOptions: {
-        args: ['--start-maximized'],
-        ignoreDefaultArgs: ['--window-size=1280,720']
-      }
-    }
-  },
 
-  {
-    name: 'Chromium',
-    use: {
-      browserName: 'chromium',
-      viewport: { width: 1920, height: 1080 },
-      launchOptions: {
-        args: [],
-        ignoreDefaultArgs: ['--window-size=1280,720']
-      }
-    }
-  },
+  // {
+  //   name: 'Microsoft Edge',
+  //   use: {
+  //     channel: 'msedge',
+  //     viewport: null,
+  //     launchOptions: {
+  //       args: ['--start-maximized'],
+  //       ignoreDefaultArgs: ['--window-size=1280,720']
+  //     }
+  //   }
+  // },
 
-  {
-    name: 'Firefox',
-    use: {
-      browserName: 'firefox',
-      viewport: { width: 1920, height: 1080 },       
-      launchOptions: {
-        args: [],
-        ignoreDefaultArgs: ['--window-size=1280,720']
-      }
-    }
-  },
+  // {
+  //   name: 'Chromium',
+  //   use: {
+  //     browserName: 'chromium',
+  //     viewport: { width: 1920, height: 1080 },
+  //     launchOptions: {
+  //       args: [],
+  //       ignoreDefaultArgs: ['--window-size=1280,720']
+  //     }
+  //   }
+  // },
 
-  {
-    name: 'WebKit',
-    use: {
-      browserName: 'webkit',
-      viewport: { width: 1920, height: 1080 },      
-      launchOptions: {
-        args: [],
-        ignoreDefaultArgs: ['--window-size=1280,720']
-      }
-    }
-  }
-    */
-], 
+  // {
+  //   name: 'Firefox',
+  //   use: {
+  //     browserName: 'firefox',
+  //     viewport: { width: 1920, height: 1080 },       
+  //     launchOptions: {
+  //       args: [],
+  //       ignoreDefaultArgs: ['--window-size=1280,720']
+  //     }
+  //   }
+  // },
+
+  // {
+  //   name: 'WebKit',
+  //   use: {
+  //     browserName: 'webkit',
+  //     viewport: { width: 1920, height: 1080 },      
+  //     launchOptions: {
+  //       args: [],
+  //       ignoreDefaultArgs: ['--window-size=1280,720']
+  //     }
+  //   }
+  // }
+],
+
 
 });
