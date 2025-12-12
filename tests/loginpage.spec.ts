8a4e54f6c4b9a17dbb1180ae840ae19269eb08a5
Varalakshmidvl <varalakshmi.doddi@gmail.com>
2025-12-12 20:35:38 +0530

    updated login

--- tests/loginpage.spec.ts
+++ tests/loginpage.spec.ts
@@ -13,10 +13,34 @@ test('@login should Invalid login', async ({ page, baseURL }) => {
     
     const loginPage = new LoginPage(page);
     await loginPage.goToLoginPage(baseURL);
-    await loginPage.doLogin('varalakshmiautomation@gmail.com', 'Hydchn66@@');
+    await loginPage.doLogin('varalakshmi.d@gmail.com', 'Hydchn66@@');
     const errorMesg = await loginPage.getInvalidLoginMessage();
     expect(errorMesg).toContain('Warning: No match for E-Mail Address and/or Password.');
 
 });
 
 
+//Due to Error Analysed and debugged the code
+
+/* test('verify valid login1', async ({ page }) => {
+
+    //AAA
+    let loginPage = new LoginPage(page);
+    await loginPage.goToLoginPage();
+    let homePage: HomePage = await loginPage.doLogin('pwtest@nal.com', 'test123');
+    expect(await homePage.isUserLoggedIn()).toBeTruthy();
+}); 
+
+
+test.skip('Verify Invalid LoginPage ', async ({ page }) => {
+  let loginPage = new LoginPage(page)
+  await loginPage.goToLoginPage()
+  const actualTitle1 = await loginPage.doLogin('varalakshmixyz@gmail.com', 'xxxHydchn66@@')
+  // const actualTitle = await loginPage.doLogin('varalakshmiautomation@gmail.com', 'Hydchn66@@')
+
+  const errorMessage = await loginPage.getInvalidLoginMessage()
+  expect(errorMessage).toContain('Warning: No match for E-Mail Address and/or Password.')
+
+
+
+}) */
